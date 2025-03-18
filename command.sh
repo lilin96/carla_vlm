e68d36016d46d01fbf940c71164dab4d74f9dd42
lilin96 <lilin96a@126.com>
2025-03-18 17:22:55 +0800

    sh

--- command.sh
+++ command.sh
@@ -1,9 +1,21 @@
 # create conda env
-conda create -y -n vlm-rl python=3.9
+conda create -y -n vlm-rl python=3.8
 conda activate vlm-rl
 
 conda install pytorch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 pytorch-cuda=12.1 -c pytorch -c nvidia
-
 pip install -r requirements.txt
-pip install -r requirements/internvl_chat.txt
 MAX_JOBS=4 pip install flash-attn==2.3.6 --no-build-isolation
+
+# Download Carla
+mkdir /home/users/ntu/songyanz/scratch/carla
+cd /home/users/ntu/songyanz/scratch/carla
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.13.tar.gz
+tar -xf CARLA_0.9.13.tar.gz
+
+# Download Internvl Model
+mkdir /home/users/ntu/songyanz/scratch/pretrained/Mini-InternVL2-4B-DA-DriveLM
+# local terminal
+rsync -avz /home/lin/proj/VLM-RL/internvl_chat/pretrained/Mini-InternVL2-4B-DA-DriveLM.zip songyanz@aspire2antu.nscc.sg:/home/users/ntu/songyanz/scratch/pretrained/Mini-InternVL2-4B-DA-DriveLM
+# on server
+cd /home/users/ntu/songyanz/scratch/pretrained/Mini-InternVL2-4B-DA-DriveLM
+unzip Mini-InternVL2-4B-DA-DriveLM.zip
